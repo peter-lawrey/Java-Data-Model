69d6e1329f8bac21c9f6de71cde037648407c9ff
Peter Lawrey <peter.lawrey@gmail.com>
2012-03-18 09:47:18 +0000

    Initial model - Person

---
+++ model-example/src/main/java/person/model/Model.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2012 Peter Lawrey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package person.model;
+
+import person.attributes.*;
+import vanilla.java.datamodel.annotations.Immutable;
+import vanilla.java.datamodel.annotations.Wrapper;
+
+public interface Model {
+    @Wrapper
+    class PersonId {
+        int value;
+    }
+
+    class Person {
+        @Immutable
+        PersonId id;
+        Name name;
+        DateOfBirth dateOfBirth;
+        Address address;
+        TaxId taxId;
+        MartialStatus martialStatus;
+        Gender gender;
+        PhoneNumber homePhone;
+        PhoneNumber mobilePhone;
+        PhoneNumber workPhone;
+    }
+}
